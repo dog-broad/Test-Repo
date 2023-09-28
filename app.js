ddb47fb5046555de4b9064807fec97c81536d47f
dog-broad <rushyendra.7004@gmail.com>
2023-09-28 18:23:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById("xlsx-file").addEventListener("change", function (event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+        alert("No file selected.");
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const html = XLSX.utils.sheet_to_html(worksheet);
+
+        document.getElementById("output").innerHTML = html;
+    };
+
+    reader.readAsArrayBuffer(file);
+});
